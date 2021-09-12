36e1a56aea650c9330e3f873e38a9ca2145614b3
Alexander Bagherzadeh <alexanderbagherzadeh@yahoo.com>
2021-09-11 21:56:11 -0400

    first commit

---
+++ exercise09/src/main/java/solution/Room.java
@@ -0,0 +1,15 @@
+package solution;
+
+public class Room {
+    private double area;
+    private static final double convertToGallons = 0.0028571429;
+
+    public Room(double length, double width) {
+        this.area = length * width;
+    }
+
+    public void getGallons() {
+        double result = area * convertToGallons;
+        System.out.printf("You will need to purchase %.0f gallons of paint to cover %.0f square feet.", Math.ceil(result), area);
+    }
+}
