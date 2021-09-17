16b9b7a9bbd5dfb516d3231d62c18a2d9898275a
Alexander Bagherzadeh <alexanderbagherzadeh@yahoo.com>
2021-09-17 17:17:13 -0400

    worked on 17

---
+++ exercise17/src/main/java/solution/Legality.java
@@ -0,0 +1,72 @@
+package solution;
+
+import java.util.Scanner;
+
+public class Legality {
+    private int gender;
+    private double amountOfAlcohol;
+    private double weight;
+    private int hoursSinceDrinking;
+    private double bacLevel;
+    private static final double bac = 0.08;
+    private static final Scanner in = new Scanner(System.in);
+
+    public void setGender() {
+        System.out.println("Enter a 1 if you are male or a 2 if you are female: ");
+        String input = in.nextLine();
+        try {
+            gender = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println(input + " is a non-numeric value.");
+        }
+    }
+
+    public void setAmountOfAlcohol() {
+        System.out.println("How many ounces of alcohol did you have? ");
+        String input = in.nextLine();
+        try {
+            amountOfAlcohol = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            System.out.println(input + " is a non-numeric value.");
+        }
+    }
+
+    public void setWeight() {
+        System.out.println("What is your weight, in pounds? ");
+        String input = in.nextLine();
+        try {
+            weight = Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            System.out.println(input + " is a non-numeric value.");
+        }
+    }
+
+    public void setHoursSinceDrinking() {
+        System.out.println("How many hours has it been since your last drink? ");
+        String input = in.nextLine();
+        try {
+            hoursSinceDrinking = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println(input + " is a non-numeric value.");
+        }
+    }
+
+    public String setBacLevel() {
+        if (gender == 1) {
+            bacLevel = ((amountOfAlcohol * 5.14) / (weight * 0.73)) - (0.015 * hoursSinceDrinking);
+            return ("Your BAC is " + bacLevel);
+        }
+        else if (gender == 2) {
+            bacLevel = ((amountOfAlcohol * 5.14) / (weight * 0.66)) - (0.015 * hoursSinceDrinking);
+            return ("Your BAC is " + bacLevel);
+        }
+        else {
+            return "Wrong input for gender.";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return (bacLevel >= bac) ? (setBacLevel() + "It ")
+    }
+}
--- exercise17/src/main/java/solution/Solution17.java
+++ exercise17/src/main/java/solution/Solution17.java
@@ -1,7 +1,12 @@
 package solution;
 
 public class Solution17 {
-    
+    //Ask user for gender
+    //Prompt user for amount of alcohol
+    //Ask user for weight
+    //Ask user for how many hours since last drink
+    //Check inputs with try catch
+    //Output BAC and if its legal to drive
     public static void main(String[] args) {
 
     }
