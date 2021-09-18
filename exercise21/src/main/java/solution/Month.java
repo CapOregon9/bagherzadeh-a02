a6020348e5009e9c81f66b93349896740f32fd9b
Alexander Bagherzadeh <alexanderbagherzadeh@yahoo.com>
2021-09-18 18:09:31 -0400

    Finished exercise 21

---
+++ exercise21/src/main/java/solution/Month.java
@@ -0,0 +1,64 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Alexander Bagherzadeh
+ */
+
+package solution;
+
+import java.util.Scanner;
+
+public class Month {
+    private int month;
+    private String output;
+    private static final Scanner in = new Scanner(System.in);
+
+    public void setMonth() {
+        do {
+            try {
+                System.out.println("Please enter the number of the month: ");
+                month = Integer.parseInt(in.nextLine());
+                if (month < 1 || month > 12) {
+                    System.out.println("Integer out of range. Try again.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("You entered non-numeric data. Try Again.");
+            }
+        } while (month < 1 || month > 12);
+    }
+
+    public String calcMonth() {
+        switch (month) {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+            default:
+                return "Incorrect month integer entered.";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("The name of the month is %s", calcMonth());
+    }
+}
--- exercise21/src/main/java/solution/Solution21.java
+++ exercise21/src/main/java/solution/Solution21.java
@@ -1,4 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Alexander Bagherzadeh
+ */
+
 package solution;
 
 public class Solution21 {
+    //Prompt user for month integer between 1 and 12
+    //determine month using switch statement
+    //output result
+    public static void main(String[] args) {
+        Month mth = new Month();
+        mth.setMonth();
+        System.out.println(mth);
+    }
 }
